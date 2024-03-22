fad8444dc241073d6844fa677e1dd95608483e62
deepa-kakade-git <deep_303@yahoo.com>
2024-03-22 09:59:31 -0400

    finished

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -3,46 +3,54 @@ import enum
 
 
 class Parity(enum.Enum):
-    ODD = 0
-    EVEN = 1
+    ODD = 1
+    EVEN = 0
 
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
 
-    :param start:
-    :param stop:
-    :param parity:
-    :return:
-    """
-    pass
+    return [i for i in range(start, stop) if i % 2 == parity.value]
 
+    # if parity == Parity.EVEN:
+    #     return [i for i in range(start, stop) if i % 2 == 0]
+    # elif parity == Parity.ODD:
+    #     return [i for i in range(start, stop) if i % 2 != 0]
+"""
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
+
+:param start:
+:param stop:
+:param parity:
+:return:
+"""
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+
+    return { i: strategy(i) for i in range (start, stop) }
+"""
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
 
 
-    :param start:
-    :param stop:
-    :param strategy:
-    :return:
-    """
-    pass
+:param start:
+:param stop:
+:param strategy:
+:return:
+"""
+
 
 
 def gen_set(val_in: str) -> Set:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    return set([letter.upper() for letter in val_in if letter.islower()])
+"""
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
 
-    :param val_in:
-    :return:
-    """
-    pass
+:param val_in:
+:return:
+"""
+
